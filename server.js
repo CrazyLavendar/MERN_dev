df16796aa18d0641eee04af67860303663576697
Jayam Vimal <crazylavendar@gmail.com>
2020-09-25 01:06:42 +0530

    Initial2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('API Running');
+  res.send('API Runnning');
 });
 const PORT = process.env.PORT || 5000;
 
